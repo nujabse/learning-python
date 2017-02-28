6a705a05a9114fb14a53b3e1c13e09d115a0f7fb
nujabse <h.p.zhumeng@gmail.com>
2017-02-28 23:07:31 +0800

    实现正则匹配获取虾米播放数据

--- hust_physical_teachers.py
+++ hust_physical_teachers.py
@@ -25,7 +25,7 @@ class hustPhysicsTeachers:
         self.name = []
         self.email = []
         self.phone = []
-        self.working_degree = []
+        self.working_degree1 = []
         # self.content = []
 
     def getpage(self, num):
@@ -75,12 +75,12 @@ class hustPhysicsTeachers:
             else:
                 print phone[0]
                 self.phone.append(phone[0])
-            if not working_degree:
-                print u"没有职称！\n"
-                self.working_degree.append("None")
-            else:
-                print working_degree[0]
-                self.working_degree.append(working_degree[0])
+            # if not working_degree:
+            #     print u"没有职称！\n"
+            #     self.working_degree.append("None")
+            # else:
+            #     print working_degree[0]
+            #     self.working_degree.append(working_degree[0])
 
     # def degreeAnalayze(self):
     #     soup = BeautifulSoup(self.getpage(self.num))
@@ -88,16 +88,31 @@ class hustPhysicsTeachers:
     #     degree = soup.find_all(text=u"职称")
     #     if degree:
     #         print degree[0]
-    def get_professors():
-        page = requests.get('http://phys.hust.edu.cn/xszdwjs/index.htm')
+
+    def get_professors(self):
+        """
+        获取教授介绍页信息，记得加self
+        :return:
+        """
+        links = ['http://phys.hust.edu.cn/xszdwjs/index.htm', 'http://phys.hust.edu.cn/xszdwfjs/index.htm', 'http://phys.hust.edu.cn/xjiangshi/index.htm', 'http://phys.hust.edu.cn/xszdwjsry/index.htm']
+        page = requests.get(links[2])
         tree = html.fromstring(page.text)
-        for i in range(1, 50):
+        for i in range(1, 70):
+
             index = '//*[@id="lbrc_1"]/ul/li[' + str(i) + ']/span/a/text()'
+            # //*[@id="lbrc_1"]/table/tbody/tr[1]/td[1]/a
             intro_raw = tree.xpath(index)
             print intro_raw
+
             for i in intro_raw:
                 intro = i.encode('utf-8')
             print intro
+            self.working_degree1.append(intro)
+
+
+
+
+
 
     def writeToExcel(self):
         """
@@ -124,10 +139,19 @@ class hustPhysicsTeachers:
             index = 'D' + str(i+2)
             ws1[index] = self.phone[i]
         print("写入电话成功")
+        for i in range(len(self.working_degree1)):
+            index = 'G' + str(i+2)
+            ws1[index] = self.working_degree1[i]
+        print("写入讲师成功")
+
+
+
         # ws1['A1'] = u"编号"
         # ws1['B1'] = u"导师"
         # ws1['C1'] = u"邮箱"
         # ws1['D1'] = u"电话"
+        # ws1['E1'] = u"职位"
+
         # for i in range(2, 200):
         #     print i
         #     index = 'A' + str(i)
@@ -144,17 +168,18 @@ class hustPhysicsTeachers:
             self.total += 1
 
     def test(self):
-        while self.num < 1054:
-            self.getpage(self.num)
-            self.statistics()
-            self.num += 1
-            print "*"*30
-            # if self.getpage(self.num):
-            #     self.degreeAnalayze()
-            print("checking page %d\n" % self.num)
-            self.getInformation()
+        # while self.num < 1054:
+            # self.getpage(self.num)
+            # self.statistics()
+            # self.num += 1
+            # print "*"*30
+            # # if self.getpage(self.num):
+            # #     self.degreeAnalayze()
+            # print("checking page %d\n" % self.num)
+            # self.getInformation()
 
-        print("总共有 %d 个老师\n" % self.total)
+        self.get_professors()
+        # print("总共有 %d 个老师\n" % self.total)
         self.writeToExcel()
         # print self.name
         # print "*" * 30
---
+++ jsDecode.py
@@ -0,0 +1,100 @@
+# -*- coding:utf-8 -*-
+import urllib2
+import re
+import sys
+from lxml import etree
+from openpyxl import load_workbook
+from openpyxl.compat import range
+import time
+time.sleep(3)   #设置时间间隔为3秒
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+counter = []
+ids =[]
+title = []
+headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36',
+           'Cookie':'unsign_token=4f23ab3b94ae95850c9c5da7aef287e2; bdshare_firstime=1460894873450; cna=aSmVD89xbyQCAcdTMxgCkCtM; gid=146233966652053; CNZZDATA921634=cnzz_eid%3D1056600907-1470359568-%26ntime%3D1472796701; CNZZDATA2629111=cnzz_eid%3D1838752247-1470359096-%26ntime%3D1472796766; _xiamitoken=d1fb6eac5ee905225c46226aef039c7b; login_method=emaillogin; member_auth=1zmQGYxLvj0xivSXS41me3AW5rCATDfXlokE27Aq5QFydYxYNYOrkKuTQApN3iSSq2FCRfnEhWQSRr0; user=22156413%22mathholic%22images%2Favatar_new%2F443%2F22156413_1394536403_1.jpg%222%2219655%22%3Ca+href%3D%27%2Fwebsitehelp%23help9_3%27+%3EDo%E2%80%A2%3C%2Fa%3E%22179%2267%2211887%2215e6c2b9dc%221488254727; _m_h5_tk=7c41b5eb2452b26db16b5b747b515b9c_1488283863638; _m_h5_tk_enc=15ff1a0b4a8465622b67a195472776fe; sec=58b57dd3547bf80f1f4a31a21817dcede3d13cc6; t_sign_auth=1; l=Ar29SsHoaUXW1EPeOTyxcTHnTRO3X/G5; isg=AoaGbUI5IiROGvZBVu5Xyu5Y13U5A8qhmkUoi3Cvd6mRcyaN2XcasWxDJRhF'
+           }
+def getdata():
+    """由于虾米部分音乐下架，可能部分音乐数据无法查证"""
+    for i in range(99, 1000):
+        try:
+            url = 'http://www.xiami.com/count/getplaycount?id=' + str(i) + '&type=song&_xiamitoken=d1fb6eac5ee905225c46226aef039c7b'  # song ID
+            song_url = 'http://www.xiami.com/song/' + str(i)
+            request = urllib2.Request(url, headers=headers)
+            response = urllib2.urlopen(request)
+            content = response.read().decode('utf-8')
+            pattern_count = re.compile('[:](\d{1,100000000})[,]')
+            count = re.findall(pattern_count, content)
+            #
+            # request_song = urllib2.Request(song_url, headers=headers)
+            # response_song = urllib2.urlopen(request_song)
+            # content_song = response_song.read().decode('utf-8')
+            # page = etree.HTML(content_song)
+            # index_song = '//*[@id="title"]/h1/text()'
+
+            # name = page.xpath(index_song)
+
+            # song = name
+            id = i
+            if counter:
+                counter.append(count[0])
+            else:
+                counter.append(None)
+            ids.append(id)
+            # title.append(name)
+            print ("song number %d  play count %s" % (i, count))
+
+
+        except urllib2.URLError, e:
+            if hasattr(e, "code"):
+                print e.code
+            if hasattr(e, "reason"):
+                print "reason", e.reason
+
+def writeToExcel():
+    """
+    存储采集数据到excel
+    :return:
+    """
+    wb = load_workbook(u"数据采集.xlsx")
+    ws1 = wb["xiami"]
+    # ws1['A1'] = u"编号"
+    # ws1['B1'] = "ids"
+    # ws1['C1'] = "title"
+    # ws1['D1'] = "counter"
+    # ws1['E1'] = "artist"
+
+    colA = ws1['A']
+    colB = ws1['B']
+    colC = ws1['C']
+    colD = ws1['D']
+
+    for i in range(len(ids)):
+        index = 'B' + str(i + 2)
+        ws1[index] = ids[i]
+    print(u"写入id.....")
+    # for i in range(len(title)):
+    #     index = 'C' + str(i + 2)
+    #     ws1[index] = title[i]
+    # print(u"写入title成功")
+    for i in range(len(counter)):
+        index = 'D' + str(i + 2)
+        ws1[index] = counter[i]
+    print(u"写入counter.....")
+    wb.save(u"数据采集.xlsx")
+    print(u"采集数据完成！")
+
+    # for i in range(len(self.working_degree1)):
+    #     index = 'G' + str(i + 2)
+    #     ws1[index] = self.working_degree1[i]
+    # print(u"写入讲师成功")
+
+getdata()
+writeToExcel()
+
+
+
+
